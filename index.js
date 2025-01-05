f81555093b6147fe1871de50114750e6cc4132d2
atemengue <moafembe@gmail.com>
2025-01-05 10:24:09 +0100

    setup phase

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+// Create a new express application instance
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(3000, () => {
+  console.log('App is running on port 3000');
+});
+
